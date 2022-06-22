f09e542cf56cbec89a7234e3c22b53d249f1fe0a
Michael Samp <michael.samp94@gmail.com>
2022-06-21 20:27:56 -0500

    cleaned and commented

--- parse.py
+++ parse.py
@@ -1,10 +1,16 @@
-from sqlite3 import Error
+"""This program takes the initally imported puzzle.db and edits the FENs
+   to start with the second move in the puzzle. This is done because the
+   tactics trainer on the Lichess website makes the first move for you so
+   you can see the last move that was made before the tactic."""
+
 import chess
 import helpers
 
+# Create connection to puzzles.db
 db = helpers.create_connection("puzzles.db")
 cursor = db.cursor()
 
+# Get all of the initial FENs and the moves for each puzzle
 original_lines = []
 
 for row in cursor.execute("SELECT PuzzleId, FEN, Moves FROM puzzles;"):
@@ -14,28 +20,27 @@ for row in cursor.execute("SELECT PuzzleId, FEN, Moves FROM puzzles;"):
     tmp.append(row[2])
     original_lines.append(tmp)
 
-
+# Iterate over every puzzle
 for line in original_lines:
-    print(line)
     
     PuzzleId = line[0]
     original_fen = line[1]
     moves = line[2]
     
+    # Make the first move in the puzzle, remove it from the move list and
+    # update the FEN
     moves = moves.split()
     board = chess.Board(original_fen)
     first_move = moves.pop(0)
     move_1 = chess.Move.from_uci(first_move)
     board.push(move_1)
     new_fen = board.fen()
-    
-    print(new_fen)
-    print(' '.join(moves))
-
     moves = ' '.join(moves)
 
+    # Update the FEN and move list for the puzzle
     cursor.execute("UPDATE puzzles SET FEN = ?, Moves = ? WHERE PuzzleId = ?;",
                     [new_fen, moves, PuzzleId])
 
+# Save database changes and close database
 db.commit()
 db.close()
--- parse2.py
+++ parse2.py
@@ -1,29 +1,46 @@
-# Create a column containing only the pieces in the FEN
+"""This program creates a new column in puzzles.db that lists the pieces for both the
+   player and the opponent in each puzzle. The convention is player pieces will be 
+   upper-case and opponent pieces will be lower-case."""
 
 import helpers
 
+# Create connection to puzzles.db
 db = helpers.create_connection('puzzles.db')
 cur = db.cursor()
 
+# Initialize list of FENs and pieces for each puzzle
 pieces = []
 fen_list = []
 counter = 0
 
+# Grab FENs from puzzle.db
 for row in cur.execute("SELECT FEN FROM puzzles;"):
     fen_list.append(row[0])
 
+# Iterate over each FEN
 for fen in fen_list:
+
+    # Create new list of pieces and split the FEN into parts on each space
     pieces.append('')
     tmp = fen.split(' ')
-    print(tmp)
+
+    # If it is black to play, swap FEN case to maintain convention of upper-case
+    # letters belonging to the player
     if tmp[1] == 'b':
         tmp[0] = tmp[0].swapcase()
+
+    # Create a list of all pieces in the FEN, not including the king
     for char in tmp[0]:
         if char.isalpha() and char not in pieces[counter] and char not in ['k', 'K']:
             pieces[counter] += char
+
+    # Sort the piece list so it can later be search for an exact match
     pieces[counter] = ''.join(sorted(pieces[counter]))
+
+    # Add the pieces column 
     cur.execute("UPDATE puzzles SET Pieces = ? WHERE FEN = ?;", [pieces[counter], fen])
     counter += 1
-    
+
+# Save database changes and close database
 db.commit()
 db.close()
